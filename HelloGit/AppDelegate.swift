782c261fdec2d6bf7a541713302ccfbaacc671ad
대웅 <dw@dwakang-mac.local>
2017-09-08 23:09:50 +0900

    Initial Commit

---
+++ HelloGit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HelloGit
+//
+//  Created by 대웅 on 2017. 9. 8..
+//  Copyright © 2017년 대웅. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HelloGit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloGit
+//
+//  Created by 대웅 on 2017. 9. 8..
+//  Copyright © 2017년 대웅. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
